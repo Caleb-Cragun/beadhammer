185719e671a6af370ba40cc6c71fbe4aaab217a7
Caleb Cragun <cpcragun@gmail.com>
2024-04-10 16:52:48 -0500

    Redo layout to have better format

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/beadhammer.jpg")
-    st.title("Why Perler Beads?")
-    why_content = """
-      When I was introduced to the hobby, I didn't have a lot of extra funds to spend on getting models. However, I did have a ton of extra perler beads (and quite a bit of extra time). So, putting my time and beads to work, I made a custom army for wargaming in the Grimdark Future. For in the grim darkness of the far flung future, there is only beads...and war.
-    """
-    st.info(why_content)
-
 
 with col2:
     st.title("What is BeadHammer?")
@@ -21,6 +15,18 @@ with col2:
       BeadHammer is a site featuring creations made of perler beads used as stand-ins for Warhammer 40k models. 
     """
     st.info(about_content)
+
+col3, col4 = st.columns(2)
+
+with col3:
+    st.title("Why Perler Beads?")
+    why_content = """
+      When I was introduced to the hobby, I didn't have a lot of extra funds to spend on getting models. However, I did have a ton of extra perler beads (and quite a bit of extra time). So, putting my time and beads to work, I made a custom army for wargaming in the Grimdark Future. For in the grim darkness of the far flung future, there is only beads...and war.
+    """
+    st.info(why_content)
+
+
+with col4:
     st.title("What can you expect to find?")
     find_content = """
       Here, I'll feature models that I've put together using ideas and designs that I've found on pinterest, as well as some self made designs. Also, I'll have a guide on making the comparable size bases for your models.
@@ -32,7 +38,7 @@ st.write("Here's a few of the models I've made.")
 
 df = pd.read_csv("data.csv", sep=",")
 
-col3, col4 = st.columns(2)
+col5, col6 = st.columns(2)
 
 def display_row(row):
   st.header(row["name"])
@@ -40,10 +46,10 @@ def display_row(row):
   st.image('images/' + row["image"])
   st.write(row["description"])
 
-with col3:
+with col5:
   for index, row in df[:5].iterrows():
     display_row(row)
 
-with col4:
+with col6:
   for index, row in df[5:].iterrows():
     display_row(row)
